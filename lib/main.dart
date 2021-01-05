7af9173d9afbc2c526cdc3336fabc28de22f509f
matthew colson <matthew.p.colson@gmail.com>
2021-01-04 21:29:56 -0600

    added network image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello, World!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text(
+            'I Am Rich',
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://weneedfun.com/wp-content/uploads/2016/07/Most-Beautiful-Sunset-Pictures-22.jpg',
+            ),
+          ),
+        ),
       ),
     ),
   );
