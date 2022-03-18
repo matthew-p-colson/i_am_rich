e35c92a05a624a684cd9be93702444b13c3907f9
matthew colson <matthew.p.colson@gmail.com>
2022-03-17 22:29:22 -0500

    Added appbar with title and centered image.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,21 @@ import 'package:flutter/material.dart';
 // Starting point of the application
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello, World!',
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text(
+            'I Am Rich',
+          ),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg',
+            ),
+          ),
         ),
       ),
     ),
